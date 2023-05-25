f72995eaca75ed4dd0f9f5b8f8a760fb437bdefa
HORINO-NOTE <y_horino@tmgt.co.jp>
2023-05-25 12:03:03 +0900

    トップページタイトル修正　広告に合わせる（断トツ安い）

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <?php include  $_SERVER['DOCUMENT_ROOT'].'/gtag_head.php'; ?>
-<?php $title = "断トツ安い！　全国建設キャリアアップ登録センター（CCUS）"; ?>
+<?php $title = "CCUS登録センター【公式】 - 断トツ安い - 知らない人はいない | 全国建設キャリアアップ登録センター（CCUS）"; ?>
 <?php $description = "全国建設キャリアアップ登録　断トツ安い代行申請　技能者登録　手続き費用が断トツ安い！　25,000円　Webで申込み"; ?>
 <?php $keyword = "CCUS,ccus,建設キャリアアップ登録,技能者登録,事業者登録"; ?>
 <?php $url = "https://www.ccus-center.com"; ?>
