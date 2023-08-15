f5e703312583dcaed5e43dc7ddfa6e98bcce4629
HORINO-NOTE <y_horino@tmgt.co.jp>
2023-08-16 08:18:19 +0900

    台風お知らせ

---
+++ form/maintenance.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<?php include  $_SERVER['DOCUMENT_ROOT'].'/gtag_head.php'; ?>
+<?php $title = "台風７号の影響による休止期間延長のお知らせ（～8/16）"; ?>
+<?php $description = "台風７号の影響による休止期間延長のお知らせ（～8/16）"; ?>
+<?php $keyword = "CCUS,ccus"; ?>
+<?php $url = "https://www.ccus-center.com/form/"; ?>
+ 
+<?php include  $_SERVER['DOCUMENT_ROOT'].'/head_settings.php'; ?>
+<link rel="stylesheet" href="/form/style_form.css" type="text/css">
+
+</head>
+
+<body id="index_php">
+<?php include_once  $_SERVER['DOCUMENT_ROOT'].'/gtag_body.php'; ?>
+ 
+ <?php include  $_SERVER['DOCUMENT_ROOT'].'/header.php'; ?>
+ 
+  <!-- contents ///////////////////////////////////-->
+ <div id="mainbody">
+  <main id="main">
+   
+  <article class="form_body">
+  <?php /* https://www.hometen.net/notes/maintenance_now/ */ ?>
+   <section id="input_kyoutu" style="margin: 20px auto; line-height: 1.5em;" class="wrapper">
+    <p>いつも全国建設キャリアアップシステム登録センターをご利用いただきまして、誠にありがとうございます。</p>
+    <p>全国建設キャリアアップシステム登録センターは台風7号の影響により、休止期間を延長させていただきます。</p>
+    <br>
+    <p>【受付休止期間】</p>
+    <p>　8月11日（金・祝）17:00　～　8月16日（水）9:00</p>
+    <br>
+    <p>ご不便、ご迷惑をおかけいたしますが、何とぞご理解いただきますようお願い申し上げます。</p>
+    <a href="/">トップページへ</a>
+   </section>
+
+  </article>
+
+  </main>
+
+</div>
+ 
+</body>
+</html>
